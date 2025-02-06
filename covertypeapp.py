cdf26a6b5795fa0119818f277e6ec41cc077113c
Tbermejo <lbermejo@unal.edu.co>
2025-02-06 14:57:20 -0500

    Update covertypeapp.py

--- covertypeapp.py
+++ covertypeapp.py
@@ -169,7 +169,7 @@ variables_range = {
     "Distancia_horizontal_a_hidrología": {"min": 0, "max": 1350, "desc": "Distancia a agua"},
     "Distancia_vertical_a_hidrología": {"min": -150, "max": 550, "desc": "Diferencia de altura con el agua"},
     "Distancia_horizontal_a_carreteras": {"min": 0, "max": 7000, "desc": "Distancia a la carretera"},
-    "Distancia horizontal a los puntos de ignición de incendios": {"min": 0, "max": 7000, "desc": "Distancia a punto de ignición"},
+    "Distancia horizontal a los puntos de ignición de incendios": {"min": 0, "max": 7000, "desc": "Distancia a punto de ignición"}
 }
 
 
